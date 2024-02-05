018ab90fac438c9adcc9f6d71356998eaf8ad856
srnatan <jonatanaradillas@gmail.com>
2024-02-05 18:09:50 +0100

    add changes

--- main.tf
+++ main.tf
@@ -6,10 +6,10 @@ resource "aws_s3_bucket" "terraform_state_cicd" {
 #   bucket = "cloudcamp-static-buckets"
 # }
 
-# terraform {
-#   backend "s3" {
-#     bucket         = "cloudcamp-terraform-state-cicd"
-#     key            = "prod-cloudcamp/terraform.tfstate"
-#     region         = "us-east-1"
-#   }
-# }
+terraform {
+  backend "s3" {
+    bucket         = "cloudcamp-terraform-state-cicd-jonatan"
+    key            = "prod-cloudcamp/terraform.tfstate"
+    region         = "us-east-1"
+  }
+}
