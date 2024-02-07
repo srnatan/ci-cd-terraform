35b0c25df23609a033e868a6e5decaa49afafa95
srnatan <jonatanaradillas@gmail.com>
2024-02-07 19:34:52 +0100

    Change bucket name

--- main.tf
+++ main.tf
@@ -1,14 +1,14 @@
 resource "aws_s3_bucket" "terraform_state_cicd" {
-  bucket = "cloudcamp-terraform-state-cicd-jonatan"
+  bucket = "my-terraform-state-bucket-jonatan"
 }
 
-resource "aws_s3_bucket" "terraform_static_buckets"{
+resource "aws_s3_bucket" "terraform_static_buckets" {
   bucket = "cloudcamp-static-buckets-jonatan"
 }
 
 terraform {
   backend "s3" {
-    bucket         = "cloudcamp-terraform-state-cicd-jonatan"
+    bucket         = "my-terraform-state-bucket-jonatan"
     key            = "prod-cloudcamp/terraform.tfstate"
     region         = "us-east-1"
   }
