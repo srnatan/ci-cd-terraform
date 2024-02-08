d911d99d719fe4f41b4e120a8a2b319a2a75af0f
srnatan <jonatanaradillas@gmail.com>
2024-02-08 16:28:30 +0100

    again

--- main.tf
+++ main.tf
@@ -1,16 +1,15 @@
-terraform {
-  backend "s3" {
-    bucket         = "cloudcamp-terraform-state-cicd-jonatanv3"
-    key            = "prod-cloudcamp/terraform.tfstate"
-    region         = "us-east-1"
-  }
-}
-
 resource "aws_s3_bucket" "terraform_state_cicd" {
-  bucket = "cloudcamp-terraform-state-cicd-jonatanv3"
+  bucket = "cloudcamp-terraform-state-cicd-jonatanv4"
 }
 
 resource "aws_s3_bucket" "terraform_static_buckets"{
   bucket = "cloudcamp-static-buckets-jonatan"
 }
 
+terraform {
+  backend "s3" {
+    bucket         = "cloudcamp-terraform-state-cicd-jonatanv4"
+    key            = "prod-cloudcamp/terraform.tfstate"
+    region         = "us-east-1"
+  }
+}
