12075c6391158cec26b74ac02e3eb64c308ca116
srnatan <jonatanaradillas@gmail.com>
2024-02-08 16:55:25 +0100

    reset terraform

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 resource "aws_s3_bucket" "terraform_state_cicd" {
-  bucket = "cloudcamp-terraform-state-cicd-jonatanv4"
+  bucket = "cloudcamp-terraform-state-cicd-jonatanv5"
 }
 
 resource "aws_s3_bucket" "terraform_static_buckets"{
@@ -8,7 +8,7 @@ resource "aws_s3_bucket" "terraform_static_buckets"{
 
 terraform {
   backend "s3" {
-    bucket         = "cloudcamp-terraform-state-cicd-jonatanv4"
+    bucket         = "cloudcamp-terraform-state-cicd-jonatanv5"
     key            = "prod-cloudcamp/terraform.tfstate"
     region         = "us-east-1"
   }
